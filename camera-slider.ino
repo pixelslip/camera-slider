b6a9785e714901f91695e845fee7b7cf73d39943
Pierre Boumal <pixelslip@gmail.com>
2015-08-18 21:54:02 +0200

    Double segment for value greater than nine

--- camera-slider.ino
+++ camera-slider.ino
@@ -1,10 +1,9 @@
-#define MAX_SPEED 120
 #define WHEEL_RADIUS 10
-#define RAIL_LENGTH 100 // cm
+#define TRANSLATE_TIME 2 // second
+#define COUNTDOWN_TIME 35 // second
 
 // set items
 int buttonPin = 2;
-int ledPin = 3;
 int motorPin = 9;
 
 // set shift register
@@ -12,104 +11,150 @@ int data = 5;
 int clock = 6;
 int latch = 7;
 
-double currentPosition; // position of camera on rails
+// set segment
+int alim_unite = 11;
+int alim_dizaine = 12;
+
+float currentPosition; // position of camera on rails
+boolean state;
 long time;
 
+void displayNumber(char nb);
+void countdown(int from);
+void startTraveling();
+void stopTraveling();
+void shiftNumber(short nb);
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(motorPin, OUTPUT);
+
+  pinMode(alim_unite, OUTPUT);
+  pinMode(alim_dizaine, OUTPUT);
 
   pinMode(data, OUTPUT);
   pinMode(clock, OUTPUT);
   pinMode(latch, OUTPUT);
 
+  state = LOW; // Allow timelapse start - at first, stop
   currentPosition = 0;
-  digitalWrite(ledPin, HIGH);
-
 }
 
 void loop()
 {
-  countdown();
+  if(state) {
+    countdown(COUNTDOWN_TIME);
+    startTraveling();
+    delay(TRANSLATE_TIME * 1000);
+    stopTraveling();
+  } else {
+    displayNumber(-2); // TODO: must display letter C instead of T
+  }
 
   if(digitalRead(buttonPin) == HIGH) {
-    startTraveling();
-  } else {
-    if((millis() - time) > 100) {
-      stopTraveling(); 
-      time = millis();
+    state = HIGH;
+    } else {
+      if((millis() - time) > 100) {
+        time = millis();
+      }
     }
-  }
 }
 
 void displayNumber(char nb) 
 {
-  digitalWrite(latch, LOW);
-  short val;
-  // TODO : Maybe could do better... It's late, my brain doesn't work well
-  switch (nb) {
-      case 0:
-        val = 191;
-        break;
-      case 1:
-        val = 249;
-        break;
-      case 2:
-        val = 164;
-        break;
-      case 3:
-        val = 48;
-        break;
-      case 4:
-        val = 25;
-        break;
-      case 5:
-        val = 18;
-        break;
-      case 6:
-        val = 2;
-        break;
-      case 7:
-        val = 248;
-        break;
-      case 8:
-        val = 0;
-        break;
-      case 9:
-        val = 16;
-        break;
-      
-      default:
-        val = 64;
+  long currentTime;
+  char unite = 0, dizaine = 0;
+
+  if(nb > 9) {
+    dizaine = nb / 10;
   }
-  shiftOut(data, clock, MSBFIRST, val);
-  digitalWrite(latch, HIGH);
+
+  unite = nb - (dizaine * 10);
+
+  currentTime = millis();
+  // set display to be able to see number
+  while((millis() - currentTime) < 1000) {
+    digitalWrite(alim_dizaine, HIGH);
+    shiftNumber(dizaine);
+    digitalWrite(alim_unite, LOW);
+    delay(10);
+
+    digitalWrite(alim_dizaine, LOW);
+    shiftNumber(unite);
+    digitalWrite(alim_unite, HIGH);
+    delay(10);
+  } 
 }
 
-void countdown() 
+void countdown(int from) 
 {
-  for( short i = 10; i > 0; i-- ) {
+  for( short i = from; i > 0; i-- ) {
     displayNumber(i);
-    delay(1000);
   }
+  displayNumber(-1); // T letter for 'translation'
 }
 
 void startTraveling() 
 {
-  if(currentPosition < RAIL_LENGTH) 
-  {
-    currentPosition += 0.001; // TODO ajouter valeur perimetre roue  
-    analogWrite(motorPin, MAX_SPEED);
-  } else {
-    digitalWrite(ledPin, LOW);
-  }
+    currentPosition += 0.001; // TODO define radius of wheel
+    digitalWrite(motorPin, HIGH);
+
+    //TODO: add detector of collision to stop train
 }
 
 void stopTraveling() 
 {
-  analogWrite(motorPin, LOW);
+  digitalWrite(motorPin, LOW);
 }
 
+void shiftNumber(short nb) 
+{
+  digitalWrite(latch, LOW);
+  short val;
+  
+  // TODO : Maybe could do better... It's late, my brain doesn't work well
+  switch (nb) {
+    case 0:
+      val = 192;
+      break;
+    case 1:
+      val = 249;
+      break;
+    case 2:
+      val = 164;
+      break;
+    case 3:
+      val = 48;
+      break;
+    case 4:
+      val = 25;
+      break;
+    case 5:
+      val = 18;
+      break;
+    case 6:
+      val = 2;
+      break;
+    case 7:
+      val = 248;
+      break;
+    case 8:
+      val = 0;
+      break;
+    case 9:
+      val = 16;
+      break;
+    case -1: // t letter
+      val = 135;
+      break;
+    case -2: // c letter
+      val = 198;
+      break;
+    default:
+      val = 192;
+  }
+
+  shiftOut(data, clock, MSBFIRST, val);
+  digitalWrite(latch, HIGH);
+}
